3d34ca365b0f305548801bb49e81a53e9000f591
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-05-27 21:44:08 +0000

    Add LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -27,6 +27,79 @@ class LinkedList {
             this.head.nextNode = current;
         }
     }
+    size(){
+        let count = 0;
+        let node = this.head;
+        while (node) {
+            count++;
+            node = node.nextNode;
+        }
+        return count;
+    }
+
+    gethead(){
+        return this.head;
+    }
+
+    tail(){
+        let node = this.head;
+        if (node) { 
+            while(node.nextNode){
+                node = node.nextNode;
+            }
+        }
+        return node;
+    }
+    at(index){
+        let node = this.head;
+        let current = null;
+        let i=1;
+        if (head === null){
+            console.log("no item in the linked list.")
+        } else {
+            while (i<=index){
+                current = node;
+                node = node.nextNode;
+                i++;
+            }
+        }   
+        return current;
+    }
+    pop(){
+        let node = this.head;
+        while (node) { 
+            if(node.nextNode.nextNode === null){
+                node.next = null;
+                return this.head;
+                
+            } else {
+                node = node.nextNode;
+            }
+            
+        }
+        
+
+    }
+    contains(value){
+        let node = this.head;
+        while (node.nextNode !=null){
+            if (node.value === value){
+                return true;
+            } 
+            node = node.nextNode;
+        }
+        if (node.value !=value){
+            return false;
+        } else{
+            return true;
+        }
+    }
+    find(value){
+
+    }
+    toString(){
+
+    }
 
 }
 
@@ -41,4 +114,12 @@ class Node {
 const list = new LinkedList();
 list.append(5); list.append('test'); list.prepend("checking");list.append("lastCh");
 list.prepend(15);
-console.log(list);
+//console.log(list);
+console.log(list.size());
+//console.log(list.gethead());
+//console.log(list.tail());
+//console.log(list.at(3));
+//console.log(list.pop());
+console.log(list.size());
+console.log(list.contains("test"));
+console.log(list.contains("tet"));
