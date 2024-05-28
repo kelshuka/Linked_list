2e9fce23ff75a364a098f5572510798870851b6d
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-05-28 13:29:19 +0000

    Add LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -94,12 +94,93 @@ class LinkedList {
             return true;
         }
     }
-    find(value){
 
+    find(value){//returns the index if found.
+        let node = this.head;
+        let i = 1;
+        while (node.nextNode !=null){
+            if (node.value === value){
+                return i;
+            } 
+            node = node.nextNode;
+            i++;
+        }
+        if (node.value !=value){
+            return null;
+        } else{
+            return i;
+        }
     }
+
     toString(){
+        //let node = new Node(value);
+        if (this.head === null) {
+            console.log("No linked list.");
+        } else {
+            let current = this.head;
+            let str = "";
+            while (current.nextNode != null) {
+                str = str + ` (${current.value}) ->`
+                current = current.nextNode;
+            }
+            str = str + ` (${current.value}) -> null`
+            return str;
+        }
 
     }
+
+    insertAt(value, index){  
+        
+        let lengt = this.size();
+        let current = this.head;
+        let previous;
+        
+        //let i=1;
+        if (index >= lengt || index <0){
+            console.log("Invalid index value");
+            //return;
+        } else if (index === 0){
+            this.prepend(value);
+            return;
+        } else if (index === lengt-1){
+            this.append(value);
+            //return;
+        }
+        while (index--){
+            previous = current;
+            current = current.nextNode;
+        }
+              
+        let node = new Node(value, previous.nextNode);
+        previous.nextNode = node;
+        node.next = current;
+    }
+
+    removeAt(index) {
+        let lengt = this.size();
+        let current = this.head;
+        let previous;
+        
+        //let i=1;
+        if (index >= lengt || index <0){
+            console.log("Invalid index value");
+            //return;
+        } else if (index === 0){
+            this.head = this.head.nextNode;
+            return;
+        } 
+        while (index--){
+            previous = current;
+            current = current.nextNode;
+        }
+        if (index === this.tail){
+            this.tail = previous;
+            //return;
+        }
+              
+        previous.nextNode = current.nextNode;   
+    }
+
    /*  clear(){
         this.head = null;
     } */
@@ -118,11 +199,14 @@ const list = new LinkedList();
 list.append(5); list.append('test'); list.prepend("checking");list.append("lastCh");
 list.prepend(15);
 //console.log(list);
-console.log(list.size());
-//console.log(list.gethead());
-//console.log(list.tail());
-//console.log(list.at(3));
-//console.log(list.pop());
-console.log(list.size());
-console.log(list.contains("test"));
-console.log(list.contains("tet"));
+//console.log(list.size());
+// console.log(list.gethead()); console.log(list.tail());
+// console.log(list.at(3)); console.log(list.pop());
+// console.log(list.contains("test")); console.log(list.contains("tet"));
+// console.log(list.find("test")); console.log(list.find("checg"));
+
+//console.log(list.toString()); list.insertAt("des",2);
+//console.log(list); console.log(list.size());console.log(list.toString());
+console.log(list);console.log(list.toString());console.log(list.size()); 
+list.removeAt(3); 
+console.log(list.size());console.log(list.toString());
